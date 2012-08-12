dcd14efbf5483c8894e264144fef2e6584ecfb3f
Curtis Larson <quackware@gmail.com>
2012-08-12 19:16:52 -0400

    Added license info

--- GIFDroid/src/com/quackware/gifdroid/ui/views/ForcedVideoView.java
+++ GIFDroid/src/com/quackware/gifdroid/ui/views/ForcedVideoView.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Curtis Larson (QuackWare).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
 package com.quackware.gifdroid.ui.views;
 
 import android.content.Context;
